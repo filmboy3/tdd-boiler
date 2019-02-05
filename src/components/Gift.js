5cdfd7b8c404377ab408d30b3cf8431adcb23225
filmboy3 <jonathanschwartz30@gmail.com>
2019-02-05 15:55:47 -0500

    added bootstrap css styling

--- src/components/Gift.js
+++ src/components/Gift.js
@@ -18,7 +18,7 @@ class Gift extends Component {
   }
   render() {
     return (
-      <div>
+      <div className="gift">
         <Form>
           <FormGroup>
             <ControlLabel>Person: </ControlLabel>
